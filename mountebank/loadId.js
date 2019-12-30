a8971cb73b6b14b136f6592d2c56e009610d4bd7
Javier Mino <javier.svg@gmail.com>
2019-12-29 19:32:30 -0600

    fix google login dynamic property

---
+++ mountebank/loadId.js
@@ -0,0 +1,55 @@
+function (config) {
+    var first = {
+        "statusCode": 302,
+        "headers": {
+          "Content-Type": "text/html; charset=UTF-8",
+          "X-Frame-Options": "DENY",
+          "Location": "https://accounts.google.com/signin/oauth/consent?authuser=0&part=AJi8hAPjiPQkVpdywGLn1aGS_eITRgM95Fn_Q0WMTlA-00dV7cZ65pEJ-MFR9fO9ghwBAjwUBnSSLawCU9_AAMJGU-0aEIm5pKp3NSE4G7kVjakgn5ISN4AbZ-1LIsrXn6wXRzyGwLjAltym5HstdfFWua5R0E8MWZJ8DU1vbavJODoNiC6WIppd8J3LWjR32mxETfIpmPvg5Fl1YBxoT-PKFRNNeUzcRiJKOyCO4yi_E8Nani7AOoreapNw-lVuirLRGR1-Gctxy2LnXe1XbntalUbAFmjESM4FFW3yQrUBIv_vQD52RuQ585BLVPG9ZFwskWQ4w8yGXKSMi7Z1XSJ0In9wVYDyTrPlhXEGClQyGmLVDB34wm9brSvhOt27aNIBolLNQiJR6nQlm0QPEDid-zV49h4UqTjuCS_xFF0hv47nGCFL0I4&as=ge23SHs6Z-wUX4XejoIWVQ&rapt=AEjHL4OC0cFclyLEYoHVp3wC-d5nRPvhSlolnpZh8IIQ-PDucPE7k1-jMwkCX0imfcwCTeVq5cTXX2jPnzd9JvmiRc_D9FiBBw",
+          "Strict-Transport-Security": "max-age=31536000; includeSubDomains",
+          "Content-Encoding": "gzip",
+          "Date": "Sun, 06 Oct 2019 14:51:53 GMT",
+          "Expires": "Sun, 06 Oct 2019 14:51:53 GMT",
+          "Cache-Control": "private, max-age=0",
+          "X-Content-Type-Options": "nosniff",
+          "X-XSS-Protection": "1; mode=block",
+          "Server": "GSE",
+          "Alt-Svc": "quic=\":443\"; ma=2592000; v=\"46,43\",h3-Q048=\":443\"; ma=2592000,h3-Q046=\":443\"; ma=2592000,h3-Q043=\":443\"; ma=2592000",
+          "Transfer-Encoding": "chunked",
+          "Connection": "close"
+        },
+        "body": "H4sIAAAAAAAAAG1TybKaUBTcv6+wTFV2PEBBIVFTzILMs26sK6CAcCGM4tfHZ7JMb06d01296dObvaupu4/NXmD413BlVxV2WjUk8cxNyrpqQJMV0wb9S3xs0H9C1uCPM1biDNWwt/Nv4hvzmSuE7mvF3ph/+eL/c3tdP9w0mcVV1JcJ7GYpaGflW7dhZntbELfztOvq9geKgiiqeti1n7equhXJZ1SVaJvdYAbRCvRdikYVbF8ev76Wvk2aLfYdlPXPGjTdllEyKmXMPDOtu1/H0yipEAeSc05k175pNCnCs4UFmlswCIbF/jo6rchaUBBNtOmrQd/SkWXy0WOh46hg5Dz6zDCaInkIBgS5JOtDvdQdgZDWdz8H9xskZUcnmMsJwVW5bUK4GkP7OUmjmjNFN5Xkvu3iqxj0gLQxgdKCk0LxHj5cwKAYfKVn3CqQ6zqmlKUa5PZyUT4E9yrXpTncSLHAj+yjchHzINq6nnjPyM6UgzFxBjFlZ4HSAczWjFE1Caj1ESn8PmtUW7JxRIq6x7RQYZjg4QV2oPAujFjmgqMRohgsJ6vxWHk4DxZPLuzeIimSVX1Tok/i2N4DixipSQoPjpatT3joKJgM6dE/8pPbmEUaChJXWJNUqj7PLomxpC+NM6RGt1gDXWarQtWtTLFX0CpKzDIFPouRp0/QKeH9dvOec84PUcTSgVhDiRNVTCbeAYJ2e0sWS2ffrk7I6IVEmOSVHPjWm21A/YpXyPcqYXBYJEbFpArHau/Xy5FDYhLa5pA6RVXA+pRSsmwhJt9HprC+40iujXcuxLLyGo2cm/i/ycgNw0VuwmdMK0OZ2dGZp8WMZcf5Lk2aZIMyu8/X8399/bsE79L8AXrPpQU8AwAA",
+        "_mode": "binary",
+        "_proxyResponseTime": 367
+    };
+    var second =  {
+        "statusCode": 200,
+        "headers": {
+          "Content-Type": "text/html; charset=utf-8",
+          "X-Frame-Options": "DENY",
+          "Cache-Control": "no-cache, no-store, max-age=0, must-revalidate",
+          "Pragma": "no-cache",
+          "Expires": "Mon, 01 Jan 1990 00:00:00 GMT",
+          "Date": "Sun, 06 Oct 2019 14:51:54 GMT",
+          "Strict-Transport-Security": "max-age=31536000; includeSubDomains",
+          "Content-Security-Policy": "script-src 'report-sample' 'nonce-T+RvY/cbHbqXhpnuzbbo9Q' 'unsafe-inline' 'unsafe-eval';object-src 'none';base-uri 'self';report-uri /cspreport",
+          "X-Content-Type-Options": "nosniff",
+          "X-XSS-Protection": "1; mode=block",
+          "Server": "GSE",
+          "Set-Cookie": "SIDCC=AN0-TYsk6o2zCxAvIPToMycUkAEa9cdRURH3JqJHXb0pVTlVYPpYfXM5h7QBrscbw3AuqClOLw; expires=Sat, 04-Jan-2020 14:51:54 GMT; path=/; domain=.google.com; priority=high",
+          "Alt-Svc": "quic=\":443\"; ma=2592000; v=\"46,43\",h3-Q048=\":443\"; ma=2592000,h3-Q046=\":443\"; ma=2592000,h3-Q043=\":443\"; ma=2592000",
+          "Transfer-Encoding": "chunked",
+          "Connection": "close"
+        },
+        "body": "<!doctype html><html lang=\"en\" dir=\"ltr\"><head><base href=\"https://accounts.google.com/\"><script data-id=\"_gd\" nonce=\"T+RvY/cbHbqXhpnuzbbo9Q\">window.WIZ_global_data = {\"Mo6CHc\":-8092549788826913677,\"OewCAd\":\"%.@.\\\"xsrf\\\",\\\"AFoagUWN8_QzL0Uk3SmI5Y1_Z9ovzGf4Aw:1570373514288\\\",[\\\"107149182550481229975\\\"]\\n,\\\"AFoagUU59vQ6WepzLInETp-rx1zVXHA3Qg:1570373514288\\\"]\\n\",\"Qzxixc\":\"S-234359864:1570373514085454\",\"W3Yyqf\":\"107149182550481229975\",\"qDCSke\":\"107149182550481229975\",\"thykhd\":\"AOfscOWmdX1mc2xo5yIL1Oi7RBNy1CBZ5GTb_-r7ZhaKKgFgTYYNBBBeQPddrda4dDSUxQ2Y0f47uVHckd5UnChxqIlC74ITAe8W6Ad7VE3iStzMRQo\\u003d\",\"w2btAe\":\"%.@.\\\"107149182550481229975\\\",\\\"107149182550481229975\\\",\\\"0\\\",false,null,null,true]\\n\"};</script><meta charset=\"utf-8\"/><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"/><link rel=\"shortcut icon\" href=\"//www.google.com/favicon.ico\"/><link rel=\"stylesheet\" href=\"https://ssl.gstatic.com/accounts/static/_/ss/k=gaia.gaiafe_glif.rBQLrd_spXA.L.W.O/am=EjXSIgQABAQAAAAAAAAAAAAACFB0yPj_W4HjNg/d=0/ct=zgms/rs=ABkqax2Uy6Dtksz-e-sYCyt64zLyeRiZEw\" type=\"text/css\"><script type=\"text/javascript\" nonce=\"T+RvY/cbHbqXhpnuzbbo9Q\">window['cssLoaded'] = true;</script><script async id=\"base-js\" src=\"https://ssl.gstatic.com/accounts/static/_/js/k=gaia.gaiafe_glif.en.Voa9j5UG7dg.O/am=EjXSIgQABAQAAAAAAAAAAAAACFB0yPj_W4HjNg/d=1/ct=zgms/rs=ABkqax3GKZhXGzVlgqayHvIWht8cMPtJ6Q/m=approvaldata\" nonce=\"T+RvY/cbHbqXhpnuzbbo9Q\"></script><script nonce=\"T+RvY/cbHbqXhpnuzbbo9Q\">var AF_initDataKeys = []\n; var AF_dataServiceRequests = {}; var AF_initDataChunkQueue = []; var AF_initDataCallback; var AF_initDataInitializeCallback; if (AF_initDataInitializeCallback) {AF_initDataInitializeCallback(AF_initDataKeys, AF_initDataChunkQueue, AF_dataServiceRequests);}if (!AF_initDataCallback) {AF_initDataCallback = function(chunk) {AF_initDataChunkQueue.push(chunk);};}</script></head><body id=\"yDmH0d\" jscontroller=\"Uas9Hd\" jsaction=\"rcuQ6b:npT2md;click:FAbpgf\"><form class=\"RFjuSb bxPAYd k6Zj8d\" jsname=\"nUpftc\" jscontroller=\"u8zR5d\" jsaction=\"rcuQ6b:WYd;\" data-credential-response=\"%.@.&quot;gf.cr&quot;,4,null,null,[4,null,null,null,[&quot;{\\n  \\&quot;scope\\&quot; : \\&quot;email profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid\\&quot;,\\n  \\&quot;id_token\\&quot; : \\&quot;eyJhbGciOiJSUzI1NiIsImtpZCI6IjhjNThlMTM4NjE0YmQ1ODc0MjE3MmJkNTA4MGQxOTdkMmIyZGQyZjMiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiMjA0NTY2ODIwMjQ2LTY3dHNlcjc0Z3R2Nzh1aWFza205NDVlbm41YjVhZ2wyLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiMjA0NTY2ODIwMjQ2LTY3dHNlcjc0Z3R2Nzh1aWFza205NDVlbm41YjVhZ2wyLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTA3MTQ5MTgyNTUwNDgxMjI5OTc1IiwiZW1haWwiOiJ0b3Vyb2ZoZXJvZXN0ZXN0QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpYXQiOjE1NzAzNzM1MTQsImV4cCI6MTU3MDM3NzExNCwianRpIjoiNTE0N2NjMmRkOWYxYmFhZDc5YzY2MDg3NDU2MGIzNjAxMTc2ZTE4ZiJ9.ceJVSEi2q2mYXM4Iq47WM0ocgjAubSXIc2UCwB4fY_mkBg-9SNDUobLzrm1yYMInP-Llz2usrNgUDG4519XgyLrSIeZfqTfBaw0UFvDQiU1qGHzTvHrpSVNhvBRnuLVd7FmlWHs6cZJa9CzK1_jBiiJR7yKE2Kl-jT98lM8DbYBSdg_BdmWZ94OMbxleuUmoetQ6K0V0kqCOu4iow3DqXpl7k-AxDdIYvizC97WkW7PDT6mMp5YfVKYVOs84KL5AEIFNSIBG4GoNp_EIMmVx2eG40w4EwqhJQhdtVfuBaeWY_As1So0sYqZSg5Hsm4WuF2X1YQLm8Ck2z-2iCekzEg\\&quot;,\\n  \\&quot;login_hint\\&quot; : \\&quot;AJDLj6KkRbfy26Znc5JcctLbuNW6ttbylKUbMTZanC-aImxQYmtmy49v9y4LPAdrGzggZTpy6aP8HwrWAubtfROQgEf66Ox_DA\\&quot;,\\n  \\&quot;client_id\\&quot; : \\&quot;204566820246-67tser74gtv78uiaskm945enn5b5agl2.apps.googleusercontent.com\\&quot;\\n}&quot;,&quot;204566820246-67tser74gtv78uiaskm945enn5b5agl2.apps.googleusercontent.com&quot;,&quot;auth661904&quot;,&quot;http://tourofheroes.com&quot;,null,false]\n]\n,null,null,[]\n]\n\" data-branding=\"jcJzye\" role=\"presentation\" novalidate><div jsname=\"uybdVe\" class=\"mbekbe bxPAYd\" role=\"presentation\"><div jsname=\"USBQqe\" class=\"iUe6Pd Us7fWe JhUD8d\" role=\"presentation\"><div class=\"PFCdh\"><img src='https://ssl.gstatic.com/accounts/ui/progress_spinner_color_20dp_4x.gif' height=\"20px\" width=\"20px\" alt=\"\"><p>One moment please...</p></div></div></div></form><div class=\"lDwpOe\"></div></body></html>",
+        "_mode": "text",
+        "_proxyResponseTime": 660
+    };
+    if(config.state.called) {
+        config.state.called = false;
+        config.logger.info('State: ' + config.logger.info(JSON.stringify(config.state, null, 4)));
+        second.body = second.body.replace('auth661904', config.state.id);
+        return second;
+    }
+    config.state.called = true;
+    return first;
+}
---
+++ mountebank/saveId.js
@@ -0,0 +1,31 @@
+function(config) {
+    var response = {
+                    "statusCode": 200,
+                    "headers": {
+                      "Content-Type": "text/html; charset=utf-8",
+                      "Cache-Control": "no-cache, no-store, max-age=0, must-revalidate",
+                      "Pragma": "no-cache",
+                      "Expires": "Mon, 01 Jan 1990 00:00:00 GMT",
+                      "Date": "Sun, 06 Oct 2019 14:51:49 GMT",
+                      "Content-Language": "en-US",
+                      "Content-Security-Policy": "script-src 'report-sample' 'nonce-+fsZyv2/BnSQ6cclWCn4Cg' 'unsafe-inline' 'strict-dynamic' https: http: 'unsafe-eval';object-src 'none';base-uri 'self';report-uri /o/cspreport",
+                      "Content-Encoding": "gzip",
+                      "Server": "ESF",
+                      "X-XSS-Protection": "0",
+                      "X-Frame-Options": "SAMEORIGIN",
+                      "Alt-Svc": "quic=\":443\"; ma=2592000; v=\"46,43\",h3-Q048=\":443\"; ma=2592000,h3-Q046=\":443\"; ma=2592000,h3-Q043=\":443\"; ma=2592000",
+                      "Transfer-Encoding": "chunked",
+                      "Connection": "close"
+                    },
+                    "body": "H4sIAAAAAAAC/4RV+2/iOBD+vX9FirQCdARCChS2hBPPvm67W8qrqFJlnCFxSeyc7QToqv/72eFxrPZONRLY45lvZr4ZD83z3vfu6PlH3/BlGLSa+29AbqspiQygNWB8jbhLqGcUi8VmaSdthiCRspGRCX/HJHEymFEJVJpyG0HG2J+cjISNLGnUKwP7iAuQTiyXZj3zHxAzc9w2uyyMkCSL4BTltu+A68HBiKIQnExCYB0xLk/01sSVvuNCQjCY6aFgEEokQYEpMArAKReMUEnCODwRoM2vglgAT09IheFYyq3AnETSEBw7WR2y+FoqCREUPSFVsLiIWVhCGLOYSlFipXq9YZdtq9YwN4IvtQ6EiCIPePFNZA3KKFYJ/LEU821ilzr06bGGcTDt0krXU95KO3dqs6vEgrlbo9Wk7CD/jToOSw7CP6HCuop54KhYDlYa94hw1jw3TWPJFZGLWEjgytAFQ0XKpTB84GCYptISchvAZ/FGASJUF/qnS4Q6bL8qA/hQaWhrjbJj7xOYs9wyplgSRnP5n2eSb3+eGWoliBtrQh3pE3GVStY+CSCXaR79tjL5na5aZJlT2sUIcU2Do/b5w5VaCw5odXUQQIKCXEarp0z0AwiVUVFVOZMvinghJM9ZhXL+aKDj+Bd9J/44O3F7inPu0DgI8tLnbG1kU5rd7M7GZThOXa05kSqVl81FdxNGRkqYOvROiVTnfkbF8IGRxH4O9rkeCdq7P2cR0FzAlJaisJB9lSzK5k+TN1R7c5nLaiYPjWJgpL4WsOsFN7vLVZkWD0jOYbNP9xjFx5n65HPK4tiwu7ZymSq1NDiELAFD+mAcK6UuuKEzlcgTuxY73pVaTXXTUjNG//xPhwJ1f+nPXWfpobOfNW8oQfuG/6zhfnub2GdMwD1s29Qdgks4YJkz9g/+pfRyfOJFjzEvAP3sX0qCeJTQlxJDsfT/xAFRtL4SV5XNta1KtVar25ZdqZm1S6nmymXFk8llPSZIrMJGpQqUVhdV5AV2EUXRAVlPoH2FUicbu4aERvTAvni6EbW5uR7PKjN4Y7fTyaO+d0FIQtNKaUUddBqzZDFnS0UZA3HEiiLOVPO/pulr9fOuP7SeaDSa9gZvo/f+FvUG1ceZiybjyfXIv10+9ILywtoEi7Avn60AudZKfrOD6bdxY4Sm89snOrmbju7Y9PFL3/5Sr39pNNp3Pd6o19t6zeZy2MEI9ZY3VcGG1fB9bL1OfUxX89f+4Haio0oJ9NyI64DKWpJWiHj63PVXbWj79VG9/Ly1HpLGtMo694P7IQfvre19c6U76HtVH990touLub8YNN7d64E1H97+Nb+eqH0QP8+G0cKuPHVmD8nz9KHc9xrJ82xioZu7CNuyiu2HBF+PswUji1fvD5fROOJRYsez9x+4F36f9nzoWp275IJ3bKQYXODO04++Vv/ebd9nC5LHUPi0X9iLmsOYsRUB9dxOhr0e83rm6//gfwAAAP//AwAu5zN5mQcAAA==",
+                    "_mode": "binary",
+                    "_proxyResponseTime": 116
+                  };
+    config.logger.info('Inside injection');
+    var redirect_uri = config.request.query['redirect_uri'];
+    var id = redirect_uri.substring(redirect_uri.indexOf('=') + 1);
+    config.logger.info(JSON.stringify(id, null, 4));
+    config.state.id = id;
+    config.logger.info('State: ' + config.logger.info(JSON.stringify(config.state, null, 4)));
+    return response;
+}
